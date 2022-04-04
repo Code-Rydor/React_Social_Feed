8a58028df82e52187e7f2a66eec1d2453c8362e4
Garrett <ga.landin89@gmail.com>
2022-04-04 15:14:19 -0500

    Created component folders and files + working on Post component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Post from './components/post/Post';
+
+
+
+
+function App() {
+
+//const [name, setName] = useState([{Name: "Garrett", Post: "How are you?"}])
+
+  return (
+    <div>
+      <Post />
+    </div>
+  );
+}
+
+export default App;
